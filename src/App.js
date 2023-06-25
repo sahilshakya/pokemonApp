9df8ff2b35e0de81e61e340322de2007b07ef508
sahil shakya <sahilshakya42@gmail.com>
2023-06-25 11:43:59 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "./component/Main";
+import "./component/style.css";
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
